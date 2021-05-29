4e1b08cd05959e1cb07975d400cad36a3d343ede
HCH <489393041@qq.com>
2021-05-29 12:08:25 +0800

    1.0

---
+++ src/main/java/com/experience/mytools/MyToolsApplication.java
@@ -0,0 +1,16 @@
+package com.experience.mytools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author hch
+ */
+@SpringBootApplication
+public class MyToolsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyToolsApplication.class, args);
+    }
+
+}
