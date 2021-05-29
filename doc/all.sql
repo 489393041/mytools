76523ef046a5503ac8035fd78381878f900ca297
HCH <489393041@qq.com>
2021-05-29 17:07:47 +0800

    1.5 集成myBatis generator，生成demo表代码

--- doc/all.sql
+++ doc/all.sql
@@ -6,6 +6,18 @@ create table `test`(
     primary key (`id`)
 )engine = innodb default charset = utf8mb4;
 
+insert into `test` values (1,'12345','测试');
 select * from test;
 
-insert into `test` values (1,'12345','测试');
+drop table if exists `demo`;
+create table `demo`(
+    `id` bigint not null comment 'id',
+    `name` varchar(50) comment '名称',
+    primary key (`id`)
+)engine = innodb default charset = utf8mb4;
+
+insert into `demo` values (1,'测试');
+select * from demo;
+
+
+
